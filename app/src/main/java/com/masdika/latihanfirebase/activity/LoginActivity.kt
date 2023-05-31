9746c800fc2f62bfa8722a66844e70083d56b372
Masdikaa <masdikailhanmansiz@gmail.com>
2023-05-31 10:41:10 +0700

    Google login function and add google-services.json

--- app/src/main/java/com/masdika/latihanfirebase/activity/LoginActivity.kt
+++ app/src/main/java/com/masdika/latihanfirebase/activity/LoginActivity.kt
@@ -1,14 +1,29 @@
 package com.masdika.latihanfirebase.activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.masdika.latihanfirebase.R
 import com.masdika.latihanfirebase.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    //Firebase auth variable
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +31,63 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Initiate firebase auth
+        auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
         binding.btnGoogleLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+
+            signInGoogle()
+
+//            startActivity(Intent(this, MainActivity::class.java))
         }
 
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResult(task)
+            }
+        }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        } else {
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("email", account.email)
+                intent.putExtra("name", account.displayName)
+                intent.putExtra("account", account.account)
+                intent.putExtra("id", account.id)
+                intent.putExtra("id token", account.idToken)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/masdika/latihanfirebase/activity/MainActivity.kt
+++ app/src/main/java/com/masdika/latihanfirebase/activity/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.masdika.latihanfirebase.activity
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import com.masdika.latihanfirebase.R
 import com.masdika.latihanfirebase.databinding.ActivityMainBinding
-import com.masdika.latihanfirebase.fragment.InsertDataFragment
-import com.masdika.latihanfirebase.fragment.ProfileFragment
-import com.masdika.latihanfirebase.fragment.ViewDataFragment
-import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,33 +15,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Inisiasi Menu Fragment
-        supportFragmentManager.beginTransaction().replace(R.id.frame_ly, ProfileFragment()).commit()
+        val email = intent.getStringExtra("email")
+        val displayName = intent.getStringExtra("name")
+        val idToken = intent.getStringExtra("id token")
 
-        binding.bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
-            override fun onTabSelected(
-                lastIndex: Int,
-                lastTab: AnimatedBottomBar.Tab?,
-                newIndex: Int,
-                newTab: AnimatedBottomBar.Tab
-            ) {
-                Log.d("Bottom_Bar", "Selected index: $newIndex, title: ${newTab.title}")
-                when (newTab.id) {
-                    // Get ID from nav_menu
-                    R.id.tab_home -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.frame_ly, ProfileFragment()).commit()
-                    }
-                    R.id.tab_insert -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.frame_ly, InsertDataFragment()).commit()
-                    }
-                    R.id.tab_view -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.frame_ly, ViewDataFragment()).commit()
-                    }
-                }
-            }
-        })
+        binding.accountEmail.text = email
+        binding.accountName.text = displayName
+        binding.accountIdToken.text = idToken
+
+        // Inisiasi Menu Fragment
+        //supportFragmentManager.beginTransaction().replace(R.id.frame_ly, ProfileFragment()).commit()
+
+//        binding.bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+//            override fun onTabSelected(
+//                lastIndex: Int,
+//                lastTab: AnimatedBottomBar.Tab?,
+//                newIndex: Int,
+//                newTab: AnimatedBottomBar.Tab
+//            ) {
+//                Log.d("Bottom_Bar", "Selected index: $newIndex, title: ${newTab.title}")
+//                when (newTab.id) {
+//                    // Get ID from nav_menu
+//                    R.id.tab_home -> {
+//                        supportFragmentManager.beginTransaction()
+//                            .replace(R.id.frame_ly, ProfileFragment()).commit()
+//                    }
+//                    R.id.tab_insert -> {
+//                        supportFragmentManager.beginTransaction()
+//                            .replace(R.id.frame_ly, InsertDataFragment()).commit()
+//                    }
+//                    R.id.tab_view -> {
+//                        supportFragmentManager.beginTransaction()
+//                            .replace(R.id.frame_ly, ViewDataFragment()).commit()
+//                    }
+//                }
+//            }
+//        })
     }
 }
