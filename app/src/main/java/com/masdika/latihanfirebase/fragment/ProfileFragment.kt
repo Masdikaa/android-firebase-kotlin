c994feaeb94b8dd38abb4e38f4b2a1a0ea2a5fcb
Masdikaa <masdikailhanmansiz@gmail.com>
2023-06-01 21:34:10 +0700

    Generate logout function in ProfileFragment.kt

--- app/src/main/java/com/masdika/latihanfirebase/fragment/ProfileFragment.kt
+++ app/src/main/java/com/masdika/latihanfirebase/fragment/ProfileFragment.kt
@@ -1,10 +1,16 @@
 package com.masdika.latihanfirebase.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.masdika.latihanfirebase.R
+import com.masdika.latihanfirebase.activity.LoginActivity
 import com.masdika.latihanfirebase.databinding.FragmentProfileBinding
 import com.squareup.picasso.Picasso
 
@@ -13,6 +19,9 @@ class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private val auth = FirebaseAuth.getInstance()
+    private val user = auth.currentUser
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +39,26 @@ class ProfileFragment : Fragment() {
         binding.accountEmail.text = getEmail.toString()
         Picasso.get().load(getImageProfile.toString()).into(binding.profilePic)
 
+        binding.btnLogOut.setOnClickListener {
+            FirebaseLogout()
+        }
+
         return binding.root
+    }
+
+    private fun FirebaseLogout() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = context?.let { GoogleSignIn.getClient(it, gso) }
+        auth.signOut()
+        googleSignInClient?.signOut()
+
+        val intent = Intent(context, LoginActivity::class.java)
+        startActivity(intent)
+        activity?.finish()
     }
 
 }
